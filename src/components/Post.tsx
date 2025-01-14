d20ba5f0b3b5b1900a9c5c1242bbea96bd4718af
Ray <rayflyworld@gmail.com>
2025-01-14 11:53:38 +0900

    adjust random index

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const names: string[] = ["Alice", "Bob", "Charlie"];
-const randomIndex = Math.floor(Math.random() * names.length);
 
 function PostComponent(): React.JSX.Element {
+  const randomIndex = Math.floor(Math.random() * names.length);
   return (
     <div>
       <p>{names[randomIndex]}</p>
