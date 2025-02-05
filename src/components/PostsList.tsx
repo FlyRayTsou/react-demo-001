700127d0a911051744f874efa563d6287dccf0ca
Ray <rayflyworld@gmail.com>
2025-02-05 11:37:32 +0900

    useState

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+
 import Post from './Post';
+import NewPost from './NewPost';
 import classes from './PostsList.module.css';
 
 function PostsList() {
-    return <ul className={classes.posts}>
-      <Post author="Mark" body="React is awesome"/>
-      <Post author="Mary" body="Full subscription"/>
-    </ul> 
+    const [ bodyValue, setBodyValue ] = useState('');
+    const [ authorValue, setAuthorValue ] = useState('');
+    function changeBodyHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        setBodyValue(event.target.value)
+    }
+    function changeAuthorHandler(event: React.ChangeEvent<HTMLInputElement>) {
+        setAuthorValue(event.target.value)
+    }
+
+    return <>
+        <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+        <ul className={classes.posts}>
+            <Post author={authorValue} body={bodyValue}/>
+            <Post author="Mary" body="Full subscription"/>
+        </ul>
+    </>
 }
 
 export default PostsList;
