6c8e4cf28ce2cddadf381d0a14f7b31eca2f830a
Ray <rayflyworld@gmail.com>
2025-02-05 14:49:37 +0900

    add modal as children

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -2,20 +2,30 @@ import { useState } from 'react';
 
 import Post from './Post';
 import NewPost from './NewPost';
+import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList() {
+    const [ isModalOpen, setIsModalOpen ] = useState(true);
     const [ bodyValue, setBodyValue ] = useState('');
     const [ authorValue, setAuthorValue ] = useState('');
+
+    function changeModalHandler() {
+        setIsModalOpen(false)
+    }
+
     function changeBodyHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
         setBodyValue(event.target.value)
     }
+    
     function changeAuthorHandler(event: React.ChangeEvent<HTMLInputElement>) {
         setAuthorValue(event.target.value)
     }
 
     return <>
-        <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+        {isModalOpen && <Modal onClose={changeModalHandler}>
+            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+        </Modal>}
         <ul className={classes.posts}>
             <Post author={authorValue} body={bodyValue}/>
             <Post author="Mary" body="Full subscription"/>
