0a16e012370bd0d55295ae2be72f3c0d4d842e92
Ray <rayflyworld@gmail.com>
2025-02-06 11:29:28 +0900

    setup for dynamic posts list

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,21 +1,39 @@
+import { useState } from 'react';
+
 import classes from './NewPost.module.css'
 
-function NewPost({onBodyChange, onAuthorChange, onCancel}:
-    {
-        onBodyChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void,
-        onAuthorChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
-        onCancel: () => void
-    }) {
+function NewPost({onCancel}: { onCancel: () => void }) {
+
+    const [ bodyValue, setBodyValue ] = useState('');
+    const [ authorValue, setAuthorValue ] = useState('');
+
+    function changeBodyHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        setBodyValue(event.target.value)
+    }
+    
+    function changeAuthorHandler(event: React.ChangeEvent<HTMLInputElement>) {
+        setAuthorValue(event.target.value)
+    }
+
+    function submitHandler(event: React.FormEvent) {
+        event.preventDefault();
+        const postData = {
+            body: bodyValue,
+            author: authorValue
+        }
+        console.log(postData)
+        onCancel();
+    }
 
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} onChange={onBodyChange} />
+                <textarea id="body" required rows={3} onChange={changeBodyHandler} />
             </p>
             <p>
                 <label htmlFor="name">Your name</label>
-                <input type="text" id="name" required onChange={onAuthorChange} />
+                <input type="text" id="name" required onChange={changeAuthorHandler} />
             </p>
             <p className={classes.actions}>
                 <button type="button" onClick={onCancel}>Cancel</button>
--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,28 +1,14 @@
-import { useState } from 'react';
-
 import Post from './Post';
 import NewPost from './NewPost';
 import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList({onCloseModal, isModalOpen}: {onCloseModal: () => void, isModalOpen: boolean}) {
-    const [ bodyValue, setBodyValue ] = useState('');
-    const [ authorValue, setAuthorValue ] = useState('');
-
-    function changeBodyHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
-        setBodyValue(event.target.value)
-    }
-    
-    function changeAuthorHandler(event: React.ChangeEvent<HTMLInputElement>) {
-        setAuthorValue(event.target.value)
-    }
-
     return <>
         {isModalOpen && <Modal onClose={onCloseModal}>
-            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} onCancel={onCloseModal} />
+            <NewPost onCancel={onCloseModal} />
         </Modal>}
         <ul className={classes.posts}>
-            <Post author={authorValue} body={bodyValue}/>
             <Post author="Mary" body="Full subscription"/>
         </ul>
     </>
